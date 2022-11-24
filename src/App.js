5ee930a6a395bca2b77a4293dc1509188591036a
tonmoy <tonmoy3673@gmail.com>
2022-11-24 10:57:04 +0600

    set layput and main file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <button className="btn btn-active">Button</button>
-<button className="btn btn-active btn-primary">Button</button>
-<button className="btn btn-active btn-secondary">Button</button>
-<button className="btn btn-active btn-accent">Button</button>
-<button className="btn btn-active btn-ghost">Button</button>
-<button className="btn btn-active btn-link">Button</button>
+      
     </div>
   );
 }
---
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h3>Navbar page</h3>
+        </div>
+    );
+};
+
+export default Navbar;

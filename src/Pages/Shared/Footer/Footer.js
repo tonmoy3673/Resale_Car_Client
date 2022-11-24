1f88a946bb3386a7a5cc1485cf68f85e9d90c8ad
tonmoy <tonmoy3673@gmail.com>
2022-11-24 16:11:01 +0600

    built footer page

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>Footer Page</h2>
+        <div className=''>
+            <div>
+                <footer className="footer p-10 bg-neutral text-neutral-content">
+                    <div>
+                        <span className="footer-title">Services</span>
+                        <Link to='/home' className="link link-hover">Home</Link>
+                        <Link to='/blog' className="link link-hover">Blog</Link>
+                        
+                        <Link className="link link-hover">Advertisement</Link>
+                    </div>
+                    <div>
+                        <span className="footer-title">Company</span>
+                        <Link className="link link-hover">About us</Link>
+                        <Link className="link link-hover">Contact</Link>
+                        
+                    </div>
+                    <div>
+                        <span className="footer-title">Legal</span>
+                        <Link className="link link-hover">Terms of use</Link>
+                        <Link className="link link-hover">Privacy policy</Link>
+                        <Link className="link link-hover">Cookie policy</Link>
+                    </div>
+                    <div>
+                        <p>Copyright © 2022 - All right reserved by <span className='font-bold'>Car Dealer Ltd</span> </p>
+                    </div>
+                </footer>
+            </div>
         </div>
     );
 };

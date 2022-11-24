552f8c8572537fe526c327608e670c22962680be
tonmoy <tonmoy3673@gmail.com>
2022-11-24 17:53:33 +0600

    set login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,84 @@
-import React from 'react';
+
+
+import { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider';
+
 
 const Login = () => {
+    const { register, handleSubmit,formState: { errors } } = useForm();
+    const {signIn}=useContext(AuthContext);
+    const [loginError,setLoginError]=useState('');
+    const location =useLocation();
+    const navigate = useNavigate ();
+
+    const from=location.state?.from?.pathname || '/';
+
+    
+    const handleLogin=data=>{
+
+        setLoginError('');
+        signIn(data.email, data.password)
+        .then(result=>{
+            const user=result.user;
+            console.log(user);
+            navigate(from,{replace:true});
+            toast.success('Login Successfully!!')
+           
+        })
+        .catch(error=>{
+            console.log(error)
+            setLoginError(error.message)
+        });
+
+        
+
+    }
     return (
-        <div>
-            <h2>This is Login page</h2>
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='text-xl'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+
+                    
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                            
+                        </label>
+                        <input type="email" {...register("email",{required:"Email Address is required"})} className="input input-bordered w-full max-w-xs"/>
+                        {errors.email && <p role="alert" className='text-red-600'>{errors.email?.message}</p>}
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                            
+                        </label>
+                        <input type="password" {...register("password",{required:"Password is required",
+                        minLength:{value:2,message:'Password must be 6 character or longer'}
+                        
+                        })} className="input input-bordered w-full max-w-xs"/>
+                        
+                        <label className="label">
+                        {errors.password && <p role="alert" className='text-red-600'>{errors.password?.message}</p>}
+                            <span className="label-text">Forget Password?</span>
+                            
+                        </label>
+                    </div>
+                    
+                   
+                    <input type="submit" className='btn btn-primary w-full' value='Login' />
+                    <div>
+                        {loginError && <p className='text-red-600'>{loginError}</p>}
+                    </div>
+                </form>
+                <p className='py-2'>New to Doctor Portal <Link to='/signup' className='text-lime-600 underline'>Create an account</Link></p>
+                <div className="divider">OR</div>
+                <button className='btn btn-outline w-full'>Login With Google</button>
+            </div>
         </div>
     );
 };
---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,129 @@
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider';
+
+const Signup = () => {
+
+    const {register,handleSubmit,formState: { errors }}=useForm();
+
+    const {createUser,updateUser} =useContext(AuthContext);
+    const [signUpError,setSignUpError]=useState('')
+    const navigate=useNavigate();
+    
+    const handleSignup=data=>{
+        setSignUpError('');
+        createUser(data.email, data.password)
+        .then(result =>{
+            const user=result.user;
+            
+            toast('User created successfully!!')
+            const userInfo={
+                displayName:(data.name)
+            }
+            updateUser(userInfo)
+            .then(()=>{
+
+                saveUser(data.name,data.email);
+            })
+            .catch(err=>{
+                console.log(err)
+                
+            });
+           
+        })
+        .catch(error=>{
+            console.log(error)
+            setSignUpError(error.message)
+        });
+
+        const saveUser=(name,email)=>{
+            const user={name,email};
+            fetch('http://localhost:5000/users',{
+                method:'POST',
+                headers:{
+                    'content-type':'application/json'
+                },
+                body:JSON.stringify(user)
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                getUserToken(email);
+            })
+        }
+
+    }
+
+    const getUserToken=email=>{
+        fetch(`http://localhost:5000/jwt?email=${email}`)
+        .then(res=>res.json())
+        .then (data=>{
+            if(data.accessToken){
+                localStorage.setItem('accessToken',data.accessToken);
+                navigate('/');
+            }
+        })
+    }
+
+    return (
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='text-xl'>Signup</h2>
+                <form onSubmit={handleSubmit(handleSignup)}>
+                <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                            
+                        </label>
+                        <input type="text" {...register("name",{
+                            required:'Name is required'
+                        })} className="input input-bordered w-full max-w-xs"/>
+                        {errors.name && <p role='alert' className='text-red-600'>{errors?.name?.message}</p>}
+                    </div>
+                    
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                            
+                        </label>
+                        <input {...register("email",{
+                            required:"Email is required"
+                        })} type="email"  className="input input-bordered w-full max-w-xs"/>
+                        {errors.email && <p role='alert' className='text-red-600'>{errors.email?.message}</p>}
+                        
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                            
+                        </label>
+                        <input type="password" 
+                        {...register("password",{
+                            required:'Password is required',
+                            minLength:{value:6,message: "Password must be 6 character long"},
+                            pattern:{value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/ , message:'Password must have uppercase number special character'}
+                        })}
+                        className="input input-bordered w-full max-w-xs" />
+                        {errors.password && <p role='alert' className='text-red-600'>{errors.password?.message}</p>}
+                        
+                    </div>
+                    
+                   
+                   <div className='py-4'>
+                   <input type="submit" className='btn btn-primary w-full' value='sing Up'/>
+                   </div>
+                </form>
+                <div>
+                    {signUpError && <p className='text-red-600'>{signUpError}</p>}
+                </div>
+                <p className='py-2'>Already have an account <Link to='/login' className='text-lime-600 underline'>Please Login</Link></p>
+                <div className="divider">OR</div>
+                <button className='btn btn-outline w-full'>Login With Google</button>
+            </div>
+        </div>
+    );
+};
+
+export default Signup;

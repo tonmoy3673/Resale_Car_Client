07a2f9875506ea6b9b0ba6a185554758bd74e380
tonmoy <tonmoy3673@gmail.com>
2023-06-02 03:43:56 +0600

    polished home page

--- src/Pages/Home/Secure/SecureInfo.js
+++ src/Pages/Home/Secure/SecureInfo.js
@@ -4,8 +4,8 @@ const SecureInfo = ({detail}) => {
     const {des,img}=detail;
     return (
         <div className='mx-auto pt-5'>
-            <div className="w-full h-full lg:w-96 card bg-base-100 shadow-xl pb-2">
-                <figure className='-mt-10'><img src={img} alt="img" className='lg:w-24 w-3/4' /></figure>
+            <div className="w-full lg:w-96 card bg-base-200 shadow-xl pb-2 mb-3">
+                <figure className='-mt-10'><img src={img} alt="img" className='lg:w-24 w-2/4' /></figure>
                 <div className="card-body">
                     <h2 className="text-center text-2xl font-semibold">{des}</h2>
                     

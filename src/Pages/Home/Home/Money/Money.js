05167a291bc1daf9e5c2bd476e45beb927f2e922
tonmoy <tonmoy3673@gmail.com>
2023-06-03 22:07:43 +0600

    built money section

--- src/Pages/Home/Home/Money/Money.js
+++ src/Pages/Home/Home/Money/Money.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 const Money = () => {
     return (
-        <div className='lg:px-20 bg-[#1B1B1D] pb-12 rounded-3xl'>
+        <div className='lg:px-20 pb-12 rounded-3xl'>
             <div className='grid gap-10 grid-cols-1 lg:grid-cols-2 justify-center items-center'>
                 <img src='https://i.ibb.co/xszfvq3/TGN-4-2.png' alt='img'>
                 </img>
 
                 <div className='text-center lg:text-start'>
-                    <h2 className='text-xl font-semibold text-white'>Tokenised Crowdfunding</h2>
+                    
                     <h1 className='text-4xl font-bold text-[#538EB6] py-4'>Create content. <br />Make money for everybody.</h1>
-                    <p className='text-md text-white'>With Fandora you can enable your loyal fan base to bid for a share of the revenue from your next product or content offering to be launched over a stipulated time period. This leads to an audience base that is even more invested in you and your work, literally and figuratively.
+                    <p className='text-md text-black'>With Car Dealer you can enable your loyal fan base to bid for a share of the revenue from your next product or content offering to be launched over a stipulated time period. This leads to an audience base that is even more invested in you and your work, literally and figuratively.
                     </p>
                     <div className='py-4 mt-3'>
                         <button className='btn btn1 py-2 uppercase text-md text-white rounded-full'>
                             JOIN THE CONVERSATION <span>
-                                <img className='ms-2' src='https://i.ibb.co/4dpGVtn/dis.png' alt='icon' />
+                                <img className='ml-2' src='https://i.ibb.co/4dpGVtn/dis.png' alt='icon' />
                             </span>
                         </button>
                     </div>
@@ -28,15 +28,15 @@ const Money = () => {
 
 
                 <div className='text-center lg:text-start'>
-                    <h2 className='text-xl font-semibold text-white'>Talent content platform</h2>
+                
                     <h1 className='text-4xl font-bold text-[#538EB6] py-4'>Build your custom <br />subscription platform</h1>
-                    <p className='text-md text-white'>Build your custom subscription platform
-                        Create your own content and subscription engine and have direct control of audience revenues. Fandora gives you access to state-of-the-art No-Code content platform which enables you to raise subscriptions as well as mint NFTs
+                    <p className='text-md text-black'>Build your custom subscription platform
+                        Create your own content and subscription engine and have direct control of audience revenues. Car Dealer gives you access to state-of-the-art No-Code content platform which enables you to raise subscriptions as well as mint NFTs
                     </p>
                     <div className='py-4 mt-3'>
                         <button className='btn btn1 py-2 uppercase text-md text-white rounded-full'>
-                            JOIN THE CONVERSATION <span>
-                                <img className='ms-2' src='https://i.ibb.co/4dpGVtn/dis.png' alt='icon' />
+                            JOIN THE CONVERSATION <span className='ml-2'>
+                                <img src='https://i.ibb.co/4dpGVtn/dis.png' alt='icon' />
                             </span>
                         </button>
                     </div>

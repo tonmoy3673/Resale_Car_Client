febe805f33c2da2b545c2f59b8f468c6c8b6261e
tonmoy <tonmoy3673@gmail.com>
2023-06-09 01:50:56 +0600

    added AOS in secure card

--- src/Pages/Home/Secure/SecureInfo.js
+++ src/Pages/Home/Secure/SecureInfo.js
@@ -3,7 +3,9 @@ import React from 'react';
 const SecureInfo = ({detail}) => {
     const {des,img}=detail;
     return (
-        <div className='mx-auto pt-5'>
+        <div className='mx-auto pt-5' data-aos="flip-left"
+        data-aos-easing="ease-out-cubic"
+        data-aos-duration="2000" data-aos-delay="1500">
             <div className="w-full lg:w-96 card bg-base-200 shadow-xl pb-2 mb-3">
                 <figure className='-mt-10'><img src={img} alt="img" className='lg:w-24 w-2/4' /></figure>
                 <div className="card-body">
